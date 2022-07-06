77b2e1107e9adf0cfbdd99dc60733ffff799de31
Aleksei Tiniakov <aleksei.tiniakov@yandex.ru>
2022-07-06 15:55:57 +0300

    modul 20 app design

--- app/src/main/java/tiniakovdev/com/MainActivity.kt
+++ app/src/main/java/tiniakovdev/com/MainActivity.kt
@@ -1,11 +1,36 @@
 package tiniakovdev.com
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.webkit.WebView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun menuToast(view: View) {
+        val toastMenu = Toast.makeText(this,"menu",Toast.LENGTH_SHORT)
+        toastMenu.show()
+    }
+    fun favoritesToast(view: View) {
+        val toastFavorites = Toast.makeText(this,"favorites", Toast.LENGTH_SHORT)
+        toastFavorites.show()
+    }
+    fun watchLaterToast(view: View) {
+        val toastWatchLater = Toast.makeText(this,"watch later", Toast.LENGTH_SHORT)
+        toastWatchLater.show()
+    }
+    fun selectionsToast(view: View) {
+        val toastSelection = Toast.makeText(this,"selections", Toast.LENGTH_SHORT)
+        toastSelection.show()
+    }
+    fun settingsToast(view: View) {
+        val toastSettings = Toast.makeText(this,"settings", Toast.LENGTH_SHORT)
+        toastSettings.show()
     }
 }
